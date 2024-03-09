c925556c6ad53005e352cca20b03fc21e66abf16
Nato Boram <nato.boram+git@gmail.com>
2024-03-08 21:31:47 -0500

    🎉 New mod!

---
+++ src/main/java/com/natoboram/lightsout/DisconnectHandler.java
@@ -0,0 +1,37 @@
+
+package com.natoboram.lightsout;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+@Environment(EnvType.SERVER)
+public class DisconnectHandler implements ServerPlayConnectionEvents.Disconnect {
+	private final Logger LOGGER;
+
+	DisconnectHandler(final Logger LOGGER) {
+		this.LOGGER = LOGGER;
+	}
+
+	@Override
+	public void onPlayDisconnect(final ServerPlayNetworkHandler handler, final MinecraftServer server) {
+		final PlayerManager playerManager = server.getPlayerManager();
+		final List<ServerPlayerEntity> playerList = playerManager.getPlayerList();
+		final int size = playerList.size();
+
+		if (size == 1) {
+			LOGGER.info("Last player disconnected, stopping server");
+			server.stop(false);
+		} else {
+			LOGGER.debug("Player disconnected, {} players remaining", size - 1);
+		}
+	}
+}
---
+++ src/main/java/com/natoboram/lightsout/LightsOut.java
@@ -0,0 +1,29 @@
+package com.natoboram.lightsout;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+
+public class LightsOut implements DedicatedServerModInitializer {
+	private static final String MOD_ID = "lightsout";
+
+	/**
+	 * This logger is used to write text to the console and the log file. It is
+	 * considered best practice to use your mod id as the logger's name. That way,
+	 * it's clear which mod wrote info, warnings, and errors.
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	/**
+	 * This code runs as soon as Minecraft is in a mod-load-ready state. However,
+	 * some things (like resources) may still be uninitialized. Proceed with mild
+	 * caution.
+	 */
+	@Override
+	public void onInitializeServer() {
+		ServerPlayConnectionEvents.DISCONNECT.register(new DisconnectHandler(LightsOut.LOGGER));
+		LOGGER.info("Loaded Lights Out!");
+	}
+}
